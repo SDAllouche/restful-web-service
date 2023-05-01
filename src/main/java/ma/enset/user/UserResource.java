ae4b0aefab740746a85ea7a0fb5621f7973d67cd
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-05-01 20:25:48 +0100

    updating

--- src/main/java/ma/enset/user/UserResource.java
+++ src/main/java/ma/enset/user/UserResource.java
@@ -1,15 +1,16 @@
 package ma.enset.user;
 
 import jakarta.validation.Valid;
-//import org.springframework.hateoas.EntityModel;
-//import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 
 @RestController
 public class UserResource {
@@ -30,7 +31,7 @@ public class UserResource {
 	
 	//EntityModel
 	//WebMvcLinkBuilder
-/*
+
 	@GetMapping("/users/{id}")
 	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
@@ -44,7 +45,7 @@ public class UserResource {
 		entityModel.add(link.withRel("all-users"));
 		
 		return entityModel;
-	}*/
+	}
 	
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
